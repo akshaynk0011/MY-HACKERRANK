9df7c5fce5bc7729ab82856a9a8ff483bc2eb575
NK NANAIAH <akshaynk0011@gmail.com>
2026-09-03 20:31:51 +0530

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 9828-simple-array-sum-Hackerrank/9828-simple-array-sum.java
@@ -0,0 +1,27 @@
+}
+        int result = simpleArraySum(ar);
+        for (String s : arStr) {
+            ar.add(Integer.parseInt(s));
+        List<Integer> ar = new ArrayList<>();
+        String[] arStr = sc.nextLine().trim().split(" ");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine().trim());
+    }
+
+            sum += x;
+        }
+        return sum;
+        int sum = 0;
+        for (int x : ar) {
+public class Solution {
+
+    static int simpleArraySum(List<Integer> ar) {
+        System.out.println(result);
+        sc.close();
+    }
+import java.util.*;
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
