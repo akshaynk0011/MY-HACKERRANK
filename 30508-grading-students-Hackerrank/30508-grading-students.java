027efba550714470cf1b647db6f2cd471b89a542
NK NANAIAH <akshaynk0011@gmail.com>
2026-09-13 22:14:16 +0530

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 30508-grading-students-Hackerrank/30508-grading-students.java
@@ -0,0 +1,28 @@
+return result;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        List<Integer> grades = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            grades.add(sc.nextInt());
+        }
+
+        List<Integer> result = gradingStudents(grades);
+
+        for (int grade : result) {
+            System.out.println(grade);
+        }
+
+        sc.close();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
